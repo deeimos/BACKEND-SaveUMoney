0589e29f84121aeb6275856aff5b410cf8117be6
deeimos <62889877+deeimos@users.noreply.github.com>
2023-05-10 10:48:39 +0300

    Доделаны счета

--- src/bills/bill.controller.ts
+++ src/bills/bill.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, HttpStatus, HttpException, Post, Res, UseGuards, Req } from '@nestjs/common';
+import { Body, Controller, UseGuards, HttpCode, HttpStatus, Req, Post, Get, Param, Patch, Delete } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Response } from 'express';
 import { BillService } from './bill.service';
 import { CreateBillDto } from './dto/createBill.dto';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { AuthService } from 'src/auth/auth.service';
+import { UpdateBillDto } from './dto/updateBill.dto';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('bills')
 export class BillController {
   constructor(
@@ -13,10 +13,36 @@ export class BillController {
     private readonly authService: AuthService,
   ) { }
 
-  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.CREATED)
   @Post()
-  async createBill(@Req() req: any, @Body() createBillDto: CreateBillDto){
+  async createBill(@Req() req: any, @Body() createBillDto: CreateBillDto) {
     const userId = req.user._id;
-    return await this.billService.createBill({...createBillDto, userId});
+    return await this.billService.createBill({ ...createBillDto, userId });
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  async getBills(@Req() req: any) {
+    const userId = req.user._id;
+    return await this.billService.findAllBills(userId);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  async getOneBill(@Param('id') id: string) {
+    return await this.billService.findOneBill(id);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  async updateBill(@Param('id') id: string, @Body() updateBillDto: UpdateBillDto) {
+    return await this.billService.updateBill(id, updateBillDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  async deleteBill(@Param('id') id: string) {
+    const deleted = await this.billService.deleteBill(id);
+    if (deleted.deletedCount) return { "bill deleted": deleted.deletedCount };
   }
 }
